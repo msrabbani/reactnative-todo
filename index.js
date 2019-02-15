7ad03cc4076f233d8b54ffdc1652d3c3bd60729c
msrabbani <msrabbanii@gmail.com>
2019-02-15 22:48:22 +0700

    fetch api test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 // import Fancy from './src/Fancy';
+import FetchAPI from './src/FetchAPI';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => FetchAPI);
