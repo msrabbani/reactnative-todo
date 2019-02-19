5ebcf4f8803894121b6effeb367858e92350b366
msrabbani <msrabbanii@gmail.com>
2019-02-19 21:12:10 +0700

    mapping actions to properties

--- src/FetchAPI.js
+++ src/FetchAPI.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TouchableOpacity} from 'react-native'
 import { connect } from 'react-redux'
+import {ADD_POST} from './reducers'
 
 const _FetchAPI = (props) => (
         <View>
                 {props.datas.map(data => 
                     <Text>{data.name}</Text>
                 )}
-
+                <TouchableOpacity onPress={props.addFetchAPIPost}>
+                    <Text>
+                        Add
+                    </Text>
+                </TouchableOpacity>
         </View>
 )
+
+const mapActionsToProps = (dispatch) => {(
+    addFetchAPIPost(data) {
+        dispatch({type: 'ADD_POST', payload: data})
+    }
+)}
 
 const mapStateToProps = (state) => ({
     datas: state.fetchAPI
 })
 
-const FetchAPI = connect(mapStateToProps)(_FetchAPI)
+export const FetchAPI = connect(mapStateToProps, mapActionsToProps)(_FetchAPI)
 
-export default FetchAPI
+
--- src/reducers.js
+++ src/reducers.js
@@ -1,16 +1,13 @@
 import { combineReducers } from 'redux'
 
-const FETCH_POSTS = 'FETCH_POSTS'
-const FETCH_POSTS_COMPLETE = 'FETCH_POSTS_COMPLETE'
+const ADD_POST = 'ADD_POST'
 
 const fetchAPI = (state = [
     {name: 'demo'}, {name: 'hello'}
 ], action) => {
     switch (action.type) {
-        case FETCH_POSTS:
-            return state
-        case FETCH_POSTS_COMPLETE:
-            return action.payload
+        case ADD_POSTS:
+            return [action.payload, ...state]
         default:
             return state
     }
